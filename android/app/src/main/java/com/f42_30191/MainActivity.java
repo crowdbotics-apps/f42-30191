5412763a7e249deadfff31bcec6945ea41c4985e
Crowdbotics <team@crowdbotics.com>
2021-08-30 06:11:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/f42_30191/MainActivity.java
@@ -0,0 +1,15 @@
+package com.f42_30191;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "f42_30191";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "f42_30191Identifier",
+  url: "https://f42_30191Identifier.botics.co",
+  api: "https://f42_30191Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
